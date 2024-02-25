0f8df38e127e245f65d186ee25d29e6cfe855e3c
Frytos <frytos@protonmail.com>
2024-02-25 22:02:21 +0100

    shortens and simplifies ex03

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -12,89 +12,43 @@
 
 #include <stdio.h>
 #include <limits.h>
-#include <stdlib.h>
 
 int	ft_atoi(char *str);
-int	ft_char_is_white_space(char *str);
-int	ft_if_sign_which(char *str);
-int	ft_if_numeric_which(char *str);
-int	ft_add_significand(long *ptr_unsigned_long, char *str);
+int	ft_add_significand(int n, char *str);
 
 int	ft_atoi(char *str)
 {
 	int		index;
 	int		sign;
-	long	unsigned_long;
-	long	*ptr_unsigned_long;
+	int		n;
 
 	index = 0;
-	while (ft_char_is_white_space(&str[index]))
-		index++;
 	sign = 1;
-	while (ft_if_sign_which(&str[index]))
+	n = 0;
+	while (str[index] == 32 || (str[index] >= 9 && str[index] <= 13))
+		index++;
+	while (str[index] == '+' || str[index] == '-')
 	{
-		sign *= ft_if_sign_which(&str[index]);
+		if (str[index] == '-')
+			sign *= -1;
 		index++;
 	}
-	unsigned_long = 0;
-	ptr_unsigned_long = &unsigned_long;
-	if (ft_if_numeric_which(&str[index]))
-		ft_add_significand(ptr_unsigned_long, &str[index]);
-	else
-		return (0);
-	return ((int)sign * unsigned_long);
+	if ('0' <= str[index] && str[index] <= '9')
+		n = ft_add_significand(n, &str[index]);
+	n = sign * n;
+	return (n);
 }
 
-int	ft_char_is_white_space(char *str)
+int	ft_add_significand(int n, char *str)
 {
-	if (str[0] == 32 || (str[0] >= 9 && str[0] <= 13))
-		return (1);
-	else
-		return (0);
-}
-
-int	ft_if_sign_which(char *str)
-{
-	if (str[0] == 45)
-		return (-1);
-	else if (str[0] == 43)
-		return (1);
-	else
-		return (0);
-}
-
-int	ft_if_numeric_which(char *str)
-{
-	if (str[0] >= 48 && str[0] <= 57)
-		return (*str);
-	else
-		return (0);
-}
-
-int	ft_add_significand(long *ptr_unsigned_long, char *str)
-{
-	int		rank;
-	int		index_rank;
-	long	to_add;
-
-	if (ft_if_numeric_which(&str[1]) && str[0] != 0)
+	n = 10 * n + (str[0] - '0');
+	if ('0' <= str[1] && str[1] <= '9')
 	{
-		rank = ft_add_significand(ptr_unsigned_long, &str[1]);
-		index_rank = rank;
-		to_add = str[0] - '0';
-		while (index_rank >= 0)
-		{
-			to_add *= 10;
-			index_rank--;
-		}
-		*ptr_unsigned_long += to_add;
-		return (rank + 1);
+		n = ft_add_significand(n, &str[1]);
+		return (n);
 	}
 	else
-	{
-		*ptr_unsigned_long += str[0] - '0';
-		return (0);
-	}
+		return (n);
 }
 
 // int	main(void)
@@ -108,16 +62,19 @@ int	ft_add_significand(long *ptr_unsigned_long, char *str)
 // 	char s7[] = "    \n \f \r \t \v   --+--2147483647"; // INT_MAX 2147483647
 // 	char s8[] = "    \n \f \r \t \v   --+-2147483648"; // INT_MIN -2147483648
 // 	char s9[] = "    \n \f \r \t \v   --+--2147483648"; // INT_MAX+1 0
-// 	char s10[] = "    \n \f \r \t \v   --+-2147483649"; // INT_MIN-1 0
-// 	printf("s1 = %d - %d\n", ft_atoi(s1), atoi(s1));
-// 	printf("s2 = %d - %d\n", ft_atoi(s2), atoi(s2));
-// 	printf("s3 = %d - %d\n", ft_atoi(s3), atoi(s3));
-// 	printf("s4 = %d - %d\n", ft_atoi(s4), atoi(s4));
-// 	printf("s5 = %d - %d\n", ft_atoi(s5), atoi(s5));
-// 	printf("s6 = %d - %d\n", ft_atoi(s6), atoi(s6));
-// 	printf("s7 = %d - %d\n", ft_atoi(s7), atoi(s7));
-// 	printf("s8 = %d - %d\n", ft_atoi(s8), atoi(s8));
-// 	printf("s9 = %d - %d\n", ft_atoi(s9), atoi(s9));
-// 	printf("s10 = %d - %d\n", ft_atoi(s10), atoi(s10));
+// 	char s10[] = "-2147483649"; // INT_MIN-1 0
+// 	printf("s1 = %d\n", ft_atoi(s1));
+// 	printf("s2 = %d\n", ft_atoi(s2));
+// 	printf("s3 = %d\n", ft_atoi(s3));
+// 	printf("s4 = %d\n", ft_atoi(s4));
+// 	printf("s5 = %d\n", ft_atoi(s5));
+// 	printf("s6 = %d\n", ft_atoi(s6));
+// 	printf("s7 = %d\n", ft_atoi(s7));
+// 	printf("s8 = %d\n", ft_atoi(s8));
+// 	printf("s9 = %d\n", ft_atoi(s9));
+// 	printf("s10 = %d\n", ft_atoi(s10));
 // 	return (0);
 // }
+// // ABCDEFGHIJKLMNOPQRSTUVWXYZ
+// // 0123456789ABCDEFGHIJKLMNOP
+// //https://jalu.ch/coding/base_converter.php
